2885697250e580c6c14ac85bba94ed3f041d6796
rodapunk <estevanmachry@gmail.com>
2024-03-17 20:46:41 -0300

    Texture Square Smile Mix

--- TextureSquareSmile/3.3.texture.fs
+++ TextureSquareSmile/3.3.texture.fs
@@ -9,5 +9,7 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
+    // reverse x-axis direction
+    //fragColor = mix(texture(texture1, texCoord), texture(texture2, vec2(1.0 - texCoord.x, texCoord.y)), 0.2);
     fragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
 }
